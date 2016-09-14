d0992ed46c0fb31aa9a526b6b267efd07beb3d7b
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-09-14 20:21:25 +0200

    provide initial implementation of single, required parameter

--- src/test/scala/arguments/ArgumentsSpec.scala
+++ src/test/scala/arguments/ArgumentsSpec.scala
@@ -2,22 +2,66 @@ package arguments
 
 import org.scalatest.FunSuite
 
+import scala.collection.{GenSeq, GenSeqLike}
+
 class ArgumentsSuite extends FunSuite {
 
-  trait Argument[T]
-  case class Parameter[T](short: Char, maybeLong: Option[String]) extends Argument[T]
-  case class ArgumentsParser[O](argument: Argument[O]) {
-    def parse[T](args: Array[String]): T = ???
+  type Builder[-I, +O] = I => Either[String, O]
+  val noopBuilder: Builder[Any, Any] = in => Right(in)
+
+  trait Reads[T] {
+    def read(s: String): T
   }
+
+  implicit val StringReader: Reads[String] = new Reads[String] {
+    override def read(s: String): String = s
+  }
+
+  sealed trait Argument[T] { self =>
+    def consume(args: Array[String]): Either[String, T]
+  }
+
+  final case class Parameter[T: Reads](short: Char) extends Argument[T] {
+    override def consume(args: Array[String]): Either[String, T] = {
+      val reader = implicitly[Reads[T]]
+      args.oIndexOf(s"-$short")
+          .map ( index => Right(index))
+          .getOrElse(Left(s"parameter '$short' is required"))
+          .right
+          .flatMap { index =>
+            if (args.isDefinedAt(index + 1)) Right(args(index + 1))
+            else Left(s"parameter '$short' requires a value" )
+          }
+          .right
+          .map( value => reader.read(value))
+
+    }
+  }
+
+  final case class ArgumentsParser[I <: Any, O <: Any](argument: Argument[I], builder: I => O) {
+    def parse(args: Array[String]): Either[String, O] = {
+
+      argument.consume(args).right.map(builder)
+    }
+  }
+
+
+
+  implicit class OptionIndexGenSeq[+A, +Repr](genSeq: Array[A]) {
+    def oIndexOf[B >: A](elem: B): Option[Int] = genSeq.indexOf(elem) match {
+      case -1 => None
+      case a => Some(a)
+    }
+  }
+
 
   test("expecting a single parameter") {
     val parser = ArgumentsParser(
-      Parameter[String]('p', Some("parameter"))
+      Parameter[String]('p'),
+      builder = (s: String) => s
     )
 
-    assert(parser.parse(Array("-p", "set")) == "set")
-
-
+    assert(parser.parse(Array("-p", "set")) == Right("set"))
 
   }
 }
