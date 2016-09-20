3c922538006b8d34ebef00b37616b8398496d7cb
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-09-20 19:15:21 +0200

    implement check for multiple occurences of same param make parser assert emptiness of remaining elemens in arguments list

--- src/test/scala/arguments/ArgumentsSpec.scala
+++ src/test/scala/arguments/ArgumentsSpec.scala
@@ -10,37 +10,68 @@ class ArgumentsSuite extends FunSuite {
   val noopBuilder: Builder[Any, Any] = in => Right(in)
 
   trait Reads[T] {
-    def read(s: String): T
+    def read(s: String): Either[String, T]
   }
 
   implicit val StringReader: Reads[String] = new Reads[String] {
-    override def read(s: String): String = s
+    override def read(s: String): Either[String, String] = Right(s)
   }
 
   sealed trait Argument[T] { self =>
-    def consume(args: Array[String]): Either[String, T]
+    def consume(args: Seq[String]): Either[String, (Seq[String], T)]
+  }
+
+  final case class Alternative[S: Reads](left: Argument[S], right: Argument[S]) extends Argument[S] {
+    override def consume(args: Seq[String]): Either[String, (Seq[String], S)]  = ???
   }
 
   final case class Parameter[T: Reads](short: Char) extends Argument[T] {
-    override def consume(args: Array[String]): Either[String, T] = {
-      val reader = implicitly[Reads[T]]
-      args.oIndexOf(s"-$short")
-          .map ( index => Right(index))
-          .getOrElse(Left(s"parameter '$short' is required"))
-          .right
-          .flatMap { index =>
-            if (args.isDefinedAt(index + 1)) Right(args(index + 1))
-            else Left(s"parameter '$short' requires a value" )
-          }
-          .right
-          .map( value => reader.read(value))
+    override def consume(args: Seq[String]): Either[String, (Seq[String], T)] = {
 
+      sealed trait State
+      case object NotFound extends State
+      case object ExpectingValue extends State
+      case object DuplicateParameter extends State
+      case class FoundValue(value: String) extends State
+
+      val reader = implicitly[Reads[T]]
+
+      val notFound = Left(s"parameter '$short' is required")
+      val requiresValue = Left(s"parameter '$short' requires a value" )
+      val paramKey = s"-$short"
+
+      val (rest, state) = args.foldLeft((List[String](), NotFound: State)){ case ((rest, state), curr) =>
+          state match {
+            case NotFound =>
+              if (curr == paramKey) (rest, ExpectingValue)
+              else (curr :: rest , NotFound)
+            case ExpectingValue => (rest, FoundValue(curr))
+            case f @ FoundValue(_) =>
+              if (curr == paramKey) (curr :: rest, DuplicateParameter)
+              else (curr :: rest, f)
+            case dp @ DuplicateParameter => (curr :: rest, dp)
+          }
+      }
+      val result = state match {
+        case NotFound => notFound
+        case ExpectingValue => requiresValue
+        case FoundValue(value) => reader.read(value)
+        case DuplicateParameter => Left(s"parameter '$short' found more than once")
+      }
+
+      result.right.map { value =>
+        (rest, value)
+      }
     }
   }
 
-  final case class ArgumentsParser[I <: Any, O <: Any](argument: Argument[I], builder: I => O) {
+  final case class ArgumentsParser[I <: Any, O <: Any](argument: Argument[I], builder: I => Either[String, O]) {
     def parse(args: Array[String]): Either[String, O] = {
-      argument.consume(args).right.map(builder)
+      val result = argument.consume(args).right
+      result.flatMap { case (rest, value) =>
+        if (!rest.isEmpty) Left(s"could not consume arguments: ${rest.mkString("")}")
+        else builder(value)
+      }
     }
   }
 
@@ -57,7 +88,7 @@ class ArgumentsSuite extends FunSuite {
   test("expecting a single parameter") {
     val parser = ArgumentsParser(
       Parameter[String]('p'),
-      builder = (s: String) => s
+      builder = (s: String) => Right(s)
     )
 
     assert(parser.parse(Array("-p", "set")) == Right("set"))
