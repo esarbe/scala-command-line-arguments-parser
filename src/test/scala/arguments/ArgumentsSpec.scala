251d389d15f1007389a4d3c97b7f9d87605ef299
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-09-22 00:49:13 +0200

    implement flag functionality

--- src/test/scala/arguments/ArgumentsSpec.scala
+++ src/test/scala/arguments/ArgumentsSpec.scala
@@ -4,6 +4,10 @@ import org.scalatest.FunSuite
 
 class ArgumentsSuite extends FunSuite {
 
+  implicit val stringReads: Reads[String] = new Reads[String] {
+    override def read(s: String): Either[String, String] = Right(s)
+  }
+
   test("expecting a single parameter") {
     val parser = ArgumentsParser(
       Parameter[String]('p'),
@@ -33,15 +37,66 @@ class ArgumentsSuite extends FunSuite {
     assert(parser.parse(Array("-q", "foo", "bar")) isLeft)
   }
 
-  case class Flag[T](c: Char) extends Argument[T] {
-    override def consume(args: Seq[String]): Either[String, (Seq[String], T)] = ???
-    override def name: String = ???
+  implicit val booleanFlagger: Flags[Boolean] = new Flags[Boolean] {
+    override def flags(present: Boolean): Either[String, Boolean] = Right(present)
   }
 
-  test("expecting flag to be present or not ") {
+  case class Flag[T: Flags](c: Char) extends Argument[T] {
+
+    sealed trait State
+    case object NotFound extends State
+    case object Found extends State
+    case object DuplicateFlag extends State
+
+    val flagger = implicitly[Flags[T]]
+
+    override def consume(args: Seq[String]): Either[String, (Seq[String], T)] = {
+      val (rest, state) = args.foldLeft((Seq[String](), NotFound: State)) { case ((rest, state), curr) =>
+        state match {
+          case NotFound =>
+            if (isFlagPresent(curr)) {
+              val remaining = stripFlag(curr)
+              if (remaining.isEmpty) (rest, Found)
+              else (rest :+ remaining, Found)
+            } else (args, NotFound)
+          case Found =>
+            if (isFlagPresent(curr)) (args, DuplicateFlag)
+            else (args, Found)
+          case DuplicateFlag => (args, DuplicateFlag)
+        }
+      }
+
+      val value = state match {
+        case Found => flagger.flags(true)
+        case NotFound => flagger.flags(false)
+        case DuplicateFlag => Left(s"duplicate flag '$c' present")
+      }
+
+      value.right.map { (rest, _)}
+    }
+
+    override def name: String = c.toString
+
+    private def isFlagPresent(arg: String): Boolean = {
+      arg.startsWith("-") && !arg.startsWith("--") && arg.contains(c)
+    }
+
+    private def stripFlag(arg: String): String = {
+      val remaining = arg.filterNot(_ == c)
+      if (remaining == "-") ""
+      else remaining
+    }
+  }
+
+  trait Flags[T] {
+    def flags(present: Boolean): Either[String, T]
+  }
+
+
+  test("expecting flag to be present or not") {
     val parser = ArgumentsParser (
       Flag[Boolean]('f'),
-      builder = (s: String) => Right(s)
+      builder = (b: Boolean) => Right(b)
     )
 
     assert(parser.parse(Array("-f")) == Right(true))
