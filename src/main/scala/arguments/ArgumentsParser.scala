5b744cd73a88430199332cd3b7820f4d2cfce1bc
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-09-26 19:27:45 +0200

    add ArgumentsParserBuilder

--- src/main/scala/arguments/ArgumentsParser.scala
+++ src/main/scala/arguments/ArgumentsParser.scala
@@ -1,6 +1,12 @@
 package arguments
 
-final case class ArgumentsParser[I, O](argument: Argument[I], builder: I => Either[String, O]) {
+final case class ArgumentsParserBuilder[I](argument: Argument[I]) {
+  def build[O](builder: Function[I, Either[String, O]]): ArgumentsParser[I, O] = {
+    ArgumentsParser(argument, builder)
+  }
+}
+
+final case class ArgumentsParser[I, O](argument: Argument[I], builder: Function[I, Either[String, O]]) {
   def parse(args: Array[String]): Either[String, O] = {
     val result = argument.consume(args).right
     result.flatMap { case (rest, value) =>
