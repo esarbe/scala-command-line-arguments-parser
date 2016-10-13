69e5a0856cf720389bd74b7a28e60c686a667a15
Raphael Bosshard <raphael.bosshard@gmail.com>
2016-10-13 18:58:42 +0200

    implement PositionalArgument

--- src/test/scala/arguments/ArgumentsSuite.scala
+++ src/test/scala/arguments/ArgumentsSuite.scala
@@ -132,9 +132,12 @@ class ArgumentsSuite extends FunSuite {
   test("expecting a positional argument") {
 
     case class PositionalArgument[T](aName: String)(implicit reads: Reads[String, T]) extends Argument[T]{
-      override def consume(args: Seq[String]): Result[(Seq[String], T)] = ???
+      override def consume(args: Seq[String]): Result[(Seq[String], T)] = args.toList match {
+        case Nil => Left(ArgumentExpected(this))
+        case head :: tail => reads.read(head).right.map((tail, _))
+      }
 
-      override def usage: String = ???
+      override def usage: String = s"< $aName >"
     }
 
     val parser = ArgumentsParserBuilder(
